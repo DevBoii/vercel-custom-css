a45372720316ad8f48da0ab18589a50343c33e0a
Fred <grfirving@gmail.com>
2021-01-22 23:12:37 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch')
 module.exports = async (req, res) => {
   const html = (await (await fetch('http://lsfd.gta.world/' + req.url)).text()).replace('</head>','<link media="all" href="/custom.css" rel="stylesheet" /></head>')
+  const html = (await (await fetch('http://lsfd.gta.world/' + req.url)).text()).replace('./','http://lsfd.gta.world/' /></head>')
   res.send(html)
   res.end()
 }
