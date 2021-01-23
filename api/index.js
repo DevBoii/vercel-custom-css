7387a9c2e5d998b4abda4cc2447f9d12f4ae2061
Fred <grfirving@gmail.com>
2021-01-23 00:32:21 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 module.exports = async (req, res) => {
-  const html = (await (await fetch('http://lsfd.gta.world/' + req.url)).text()).replace('</head>','<link media="all" href="/custom.css" rel="stylesheet" /></head>').replace(/https:\/\/lsfd.gta.world\//g,'https://vercel-custom-css-jkwrg0zu4.vercel.app/').replace(/.\/..\/assets/g,'https://lsfd.gta.world/')
+  const html = (await (await fetch('http://lsfd.gta.world/' + req.url)).text()).replace('</head>','<link media="all" href="/custom.css" rel="stylesheet" /></head>').replace(/https:\/\/lsfd.gta.world\//g,'https://vercel-custom-css-jkwrg0zu4.vercel.app/').replace(/.\/..\/styles/g,'https://lsfd.gta.world/')
   res.send(html)
   res.end()
 }
